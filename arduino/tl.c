e037ef5681c159b16c2f003f6793ab4d9f4a1fcf
Ruslan Fatkullin <github.com/fatkullin>
2017-03-17 23:45:34 +0300

    trafic light managed mode implemented

--- arduino/tl.c
+++ arduino/tl.c
@@ -2,10 +2,13 @@
 #include <util/delay.h>
 
 #define DELAY_MS 100
-#define LED_PIN 	(1 << 5)
+#define BLINK_TACTS 3	
+
 #define SIGNAL_RED 	(1 << 5) // arduino pin 13
 #define SIGNAL_YELLOW 	(1 << 4) // arduino pin 12
 #define SIGNAL_GREEN 	(1 << 3) // arduino pin 11
+#define SIGNAL_ALL (SIGNAL_RED | SIGNAL_YELLOW | SIGNAL_GREEN)
+
 
 #define FLAG_BLINKED 	(1)	 // should not be equal to any signal
 
@@ -43,25 +46,25 @@ int StateTime(State_t state)
 		return 10;
 	}
 }
-void SwitchOnZeroTime(State_t newState);
-void BlinkOrSwitch(State_t newState);
+void SwitchOnZeroTimeTo(State_t newState);
+void BlinkOrSwitchTo(State_t newState);
 
 void TrySwithState()
 {
 	switch (g_state)
 	{
 	case stRed:
-		return SwitchOnZeroTime(stRedYellow);
+		return SwitchOnZeroTimeTo(stRedYellow);
 	case stRedYellow:
-		return SwitchOnZeroTime(stGreen);
+		return SwitchOnZeroTimeTo(stGreen);
 	case stGreen:
-		return SwitchOnZeroTime(stGreenBlinked);
+		return SwitchOnZeroTimeTo(stGreenBlinked);
 	case stGreenBlinked:
-		return BlinkOrSwitch(stYellow);
+		return BlinkOrSwitchTo(stYellow);
 	case stYellow:
-		return SwitchOnZeroTime(stRed);
+		return SwitchOnZeroTimeTo(stRed);
 	case stYellowBlinked:
-		return BlinkOrSwitch(stYellowBlinked);
+		return BlinkOrSwitchTo(stYellowBlinked);
 	}
 }
 	
@@ -77,39 +80,49 @@ void Show(State_t state)
 	PORTB |= (state & ~FLAG_BLINKED);
 }
 
-void SwitchOnZeroTime(State_t newState)
+void SwitchTo(State_t newState)
+{
+	Hide(g_state);
+	Show(newState);
+	g_time = StateTime(newState);
+	g_state = newState;
+}
+
+void SwitchOnZeroTimeTo(State_t newState)
 {
 	if (g_time == 0)
 	{
-		Hide(g_state);
-		Show(newState);
-		g_time = StateTime(newState);
-		g_state = newState;
+		SwitchTo(newState);
 	}
 }
 
-void BlinkOrSwitch(State_t newState)
+void BlinkOrSwitchTo(State_t newState)
 {
 	if (g_time == 0)
-		return SwitchOnZeroTime(newState);
-	
-	if (g_time % 4 == 0)		// show every 4 tick
-		Show(g_state);
+		return SwitchTo(newState);
+
+	if (g_time % (BLINK_TACTS * 2) == 0)	// light every 2 tacts
+		return Show(g_state);
 		
-	if (g_time % 2 == 0)		// hide since 2 tick after show
+	if (g_time % BLINK_TACTS  == 0)		// dark since 1 tact after light
 		Hide(g_state);
+}
+
+void OnTimer()
+{
+	g_time -= 1;
+	TrySwithState();
 }
 
 int main ()
 {
-    DDRB |= SIGN;
-    while (1)
-    {
-	PORTB |= LED_PIN;
-	_delay_ms(1000);
-	PORTB &= ~LED_PIN;
-	_delay_ms(1000);	
-    }
-    return 0;
+	DDRB |= SIGNAL_ALL;
+	SwitchTo(SIGNAL_RED);
+	while (1)
+	{
+		_delay_ms(DELAY_MS);
+		OnTimer();
+	}
+	return 0;
 }
 
